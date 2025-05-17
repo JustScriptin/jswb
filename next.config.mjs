c4da7021fcba9b8b99a4641aa870f0913aa0c2b2
Justin Padilla <73454652+JustScriptin@users.noreply.github.com>
2025-05-17 14:26:24 -0400

    chore: rename next config to mjs for mdx support

---
+++ next.config.mjs
@@ -0,0 +1,16 @@
+import createMDX from "@next/mdx";
+import remarkGfm from "remark-gfm";
+import rehypeSanitize from "rehype-sanitize";
+
+const withMDX = createMDX({
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [rehypeSanitize],
+  },
+});
+
+const nextConfig = {
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+};
+
+export default withMDX(nextConfig);
