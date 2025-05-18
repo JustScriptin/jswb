9f869b20d03f860e91bbef44ee7c9142a39c1910
Justin Padilla <73454652+JustScriptin@users.noreply.github.com>
2025-05-17 20:39:55 -0400

    test: add jest setup and sample tests

---
+++ src/hooks/useDebounce.test.ts
@@ -0,0 +1,26 @@
+import { renderHook, act } from "@testing-library/react"
+import { useDebounce } from "./useDebounce"
+
+describe("useDebounce", () => {
+  jest.useFakeTimers()
+
+  it("updates value after delay", () => {
+    const { result, rerender } = renderHook(({ value }) => useDebounce(value, 200), {
+      initialProps: { value: "a" },
+    })
+
+    expect(result.current).toBe("a")
+
+    rerender({ value: "b" })
+    act(() => {
+      jest.advanceTimersByTime(100)
+    })
+    // still old value
+    expect(result.current).toBe("a")
+
+    act(() => {
+      jest.advanceTimersByTime(100)
+    })
+    expect(result.current).toBe("b")
+  })
+})
